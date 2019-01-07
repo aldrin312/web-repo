49de07ef10a5dc981cd672a2b3b6730240270f6b
aldrin312 <aldrin_fernandez312@yahoo.com>
2019-01-07 18:17:41 -0500

    int push

---
+++ Scripts/core/game.js
@@ -0,0 +1,12 @@
+// IFFE -Immediteate INvoke Funciotns Expression
+/*
+closure
+calls an anonymous self-executing function
+anything n braces is in a closure. wont go to global namespace
+*/
+
+(function(){
+    function Init() {
+        console.log("initialization start");
+    }
+})();
