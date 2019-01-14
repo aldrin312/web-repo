ef241e918c67b58a2f5a13a7b188e16099bfdbdf
aldrin312 <aldrin_fernandez312@yahoo.com>
2019-01-14 17:55:52 -0500

    added yarn and lite-server

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -5,9 +5,32 @@ calls an anonymous self-executing function
 anything n braces is in a closure. wont go to global namespace
 */
 (function () {
+    //global game variables
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
     function Init() {
         console.log("initialization start");
-        var x = 10;
+        Start();
+    }
+    function Start() {
+        console.log("starting application");
+        //initialize creatJS
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; //60 fps;
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        helloLabel.rotation += 5;
+        stage.update();
+    }
+    function Main() {
+        console.log("game started");
+        helloLabel = new createjs.Text("hello world 123", "40px Consolate", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
     }
     window.onload = Init;
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -7,13 +7,42 @@ anything n braces is in a closure. wont go to global namespace
 */
 
 (function(){
-    function Init() {
+
+    //global game variables
+    let canvas = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel:createjs.Text;
+
+    function Init():void {
         console.log("initialization start");    
 
-        let x: number = 10;
-
+        Start();
     }
 
+    function Start():void{
+        console.log("starting application");
+
+        //initialize creatJS
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; //60 fps;
+        createjs.Ticker.on("tick",Update);
+        Main();
+
+    }
+    function Update():void{
+        helloLabel.rotation +=5;
+        stage.update();
+
+    }
+    function Main():void{
+        console.log("game started");
+        helloLabel = new createjs.Text("hello world 123", "40px Consolate", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+
+        stage.addChild(helloLabel);
+
+    }
     window.onload =Init;
 
 })();   
