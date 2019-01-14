535564332991391c02ff90ccdcc1f7ffecc21cca
aldrin312 <aldrin_fernandez312@yahoo.com>
2019-01-14 16:31:50 -0500

    typescript

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,14 +1,19 @@
+
 // IFFE -Immediteate INvoke Funciotns Expression
 /*
 closure
 calls an anonymous self-executing function
 anything n braces is in a closure. wont go to global namespace
 */
-(function () {
+
+(function(){
     function Init() {
-        console.log("initialization start");
-        var x = 10;
+        console.log("initialization start");    
+
+        let x: number = 10;
+
     }
-    window.onload = Init;
-})();
-//# sourceMappingURL=game.js.map
+
+    window.onload =Init;
+
+})();   
